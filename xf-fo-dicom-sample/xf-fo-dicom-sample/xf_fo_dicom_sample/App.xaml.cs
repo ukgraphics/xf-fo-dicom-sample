6f1d31bdc017645213b75103b632648c6e6a495d
Kuni <kuni.ujiie@grapecity.com>
2017-10-24 11:39:58 +0900

    Initial Commit

---
+++ xf-fo-dicom-sample/xf-fo-dicom-sample/xf_fo_dicom_sample/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xf_fo_dicom_sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new xf_fo_dicom_sample.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ xf-fo-dicom-sample/xf-fo-dicom-sample/xf_fo_dicom_sample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Dicom;
+using Dicom.Imaging;
+using Dicom.Log;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xf_fo_dicom_sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("xf_fo_dicom_sample.jpeg-baseline.dcm");
+            //Stream stream = assembly.GetManifestResourceStream("xf_fo_dicom_sample.CR_JPG_IR6a.dcm");
+
+            var imageFile = DicomFile.Open(stream);
+            var dicomImage = new DicomImage(imageFile.Dataset);
+
+            byte[] byteArray = dicomImage.PixelData.GetFrame(0).Data;
+            MemoryStream ms = new MemoryStream(byteArray);
+            ImageSource imgSource = ImageSource.FromStream(() => ms);
+            renderimage.Source = imgSource;
+
+            var dump = imageFile.WriteToString();
+            dicominfo.Text = dump;
+        }
+    }
+}
